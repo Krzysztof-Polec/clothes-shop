a598ca3ba6e15475499d3526dd9064a9a42b5ba1
Krzysztof Połeć <krzysp5592@gmail.com>
2024-05-05 00:00:15 +0200

    added content and styles to the about us page

--- client/src/pages/AboutUsPage/AboutUsPage.jsx
+++ client/src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,7 +1,21 @@
-const AboutUsPage = () => {
+import styles from "./AboutUsPage.module.scss"
+
+const AboutUs = () => {
   return(
-    <div>o nas</div>
+    <div className={styles.aboutUsPage}>
+      <div className={styles.aboutUsPageContent}>
+        <h1>O nas</h1>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse fugiat nulla pariatur.</p>
+        <p>
+          <span>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</span>
+          mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore 
+          veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem 
+          quia voluptas sit aspernatur aut odit aut fugit.
+        </p>
+      </div>
+    </div>
   )
 }
 
-export default AboutUsPage
+export default AboutUs
