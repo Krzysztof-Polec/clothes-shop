fe727eaef82a0e7979daa3b73f8e5ac788cb609e
Krzysztof Połeć <krzysp5592@gmail.com>
2024-05-04 23:41:42 +0200

    added content and styles to the contact page

--- client/src/pages/ContactPage/ContactPage.jsx
+++ client/src/pages/ContactPage/ContactPage.jsx
@@ -1,7 +1,37 @@
+import styles from "./ContactPage.module.scss"
+
 const ContactPage = () => {
   return(
-    <div>
-      kontakt
+    <div className={styles.contactPage}>
+      <div className={styles.contactPageContent}>
+        <h1>Kontakt</h1>
+        <div className={styles.contactPageContentTop}>
+          <div>
+            <p>adres</p>
+            <p>9606 North MoPac Expressway Suite 700 Austin, TX 78759</p>
+          </div>
+          <div>
+            <p>nr telefonu</p>
+            <p>+1 248-785-8545</p>
+          </div>
+          <div>
+            <p>email</p>
+            <p>shop@gmail.com</p>
+          </div>
+        </div>
+        <div className={styles.contactPageContentBottom}>
+          <div>
+            <p>Marketing</p>
+            <p>Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore ut eniam</p>
+            <p>shop@gmail.com</p>
+          </div>
+          <div>
+            <p>Kariery</p>
+            <p>Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore ut eniam</p>
+            <p>shop@gmail.com</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
